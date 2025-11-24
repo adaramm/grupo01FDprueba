49c07a9ab2c65f4a5c2c9540235396906f6a7ee9
alyson-cortes <cortes.alyson1c@gmail.com>
2025-11-24 08:52:42 -0600

    Update snake.h

--- snake.h
+++ snake.h
@@ -5,6 +5,7 @@ void imprimir_adara();
 void imporimir_ana();
 void imprimir_josue();
 void imporimir_ana();
+void imprimir_alyson();
 void imprimir_luis_felipe();
 void imprimir_fernanda();
 void imprimir_daniel();
